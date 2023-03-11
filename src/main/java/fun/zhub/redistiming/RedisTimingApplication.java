6b931f07d7f45317676c66ab73d51a0ff9e2067f
zaki <cnzakii@gmail.com>
2023-03-11 17:25:42 +0800

    Initial commit

---
+++ src/main/java/fun/zhub/redistiming/RedisTimingApplication.java
@@ -0,0 +1,23 @@
+package fun.zhub.redistiming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *  主启动类
+ * <p>
+ *
+ * @author Zaki Chen
+ * @version 1.0
+ * @since 2023-03-08
+ **/
+
+@SpringBootApplication
+public class RedisTimingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisTimingApplication.class, args);
+    }
+
+}
